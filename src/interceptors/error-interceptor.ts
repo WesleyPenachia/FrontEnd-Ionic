a8d5811d518477bd3a8e071e2d4cea83460a7e6d
Wesley Penachia <wesleyikiko@hotmail.com>
2022-04-17 20:35:24 -0300

    Tratamento padrão com alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,48 +1,77 @@
-import { Injectable } from "@angular/core";
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { Observable} from "rxjs";
-import { catchError } from 'rxjs/operators';
-import { StorageService } from "../services/storage.service";
- 
- 
-@Injectable()
-export class ErrorInterceptor implements HttpInterceptor{
-  
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
-    constructor(public storage: StorageService) {
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
     }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-       // console.log ("Passou")
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
-                .pipe(
-                    catchError(error => {
-                        let errorObj = error;
-                        if(errorObj.error ){
- 
-                           errorObj = errorObj.error;
-                        }
-                        if(!errorObj.status){
-                            errorObj = JSON.parse(errorObj.error);
-                        }
-                        //alert("erro inteceptor");
-                        console.log("error do Inteceptor");
-                        console.log(errorObj);
+        .catch((error, caught) => {
+            let errorObj = error;
+            if (errorObj.error) {
+                errorObj = errorObj.error;
+            }
+            if (!errorObj.status) {
+                errorObj = JSON.parse(errorObj);
+            }
+            console.log("Erro detectado pelo interceptor:");
+            console.log(errorObj);
 
-                        switch(errorObj.status) {
-                            case 403:
-                            this.handle403();
-                            break;
-                        }
-                       
-                        return Observable.throw(errorObj);
-                    })) as any;
+            switch(errorObj.status) {
+                case 401:
+                this.handle401();
+                break;
+
+                case 403:
+                this.handle403();
+                break;
+
+                default:
+                this.handleDefaultEror(errorObj);
+            }
+
+            return Observable.throw(errorObj);
+        }) as any;
     }
     handle403() {
         this.storage.setLocalUser(null);
     }
-} 
- 
+
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultEror(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();        
+    }
+}
+
 export const ErrorInterceptorProvider = {
     provide: HTTP_INTERCEPTORS,
     useClass: ErrorInterceptor,
